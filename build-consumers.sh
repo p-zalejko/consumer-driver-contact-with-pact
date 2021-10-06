d9f82037f003afd6223c5adf6de8ffd848e775e7
Pawel Zalejko <p.zalejko@gmail.com>
2021-10-06 21:50:32 +0200

    pact c.d.

--- build-consumers.sh
+++ build-consumers.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+GIT_HASH_SHORT=`git rev-parse --short HEAD`
+export GIT_COMMIT_HASH_SHORT=$GIT_HASH_SHORT
 mvn clean test pact:publish -f ./pact-consumer-foo/pom.xml
-mvn clean test pact:publish -f ./pact-consumer-bar/pom.xml
+mvn clean test pact:publish -f ./pact-consumer-bar/pom.xml 
